37a26afb34101edac1aeac51d89e01e067b776c4
ziorye <ziorye@gmail.com>
2023-09-19 20:54:43 +0800

    @BeforeAll

--- day0915-spring-quickstart/src/test/java/service/SpringIoCTest.java
+++ day0915-spring-quickstart/src/test/java/service/SpringIoCTest.java
@@ -3,16 +3,22 @@ package service;
 import dao.impl.NopeUserDao;
 import dao.impl.SimpleUserDao;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import service.impl.UserServiceImpl;
 
 class SpringIoCTest {
+    public static AbstractApplicationContext ac;
+
+    @BeforeAll
+    static void initApplicationContext() {
+        ac = new ClassPathXmlApplicationContext("beans.xml");
+    }
 
     @Test
     void ioc() {
-        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("beans.xml");
-
         Assertions.assertTrue(ac.containsBean("userService"));
         Assertions.assertTrue(ac.containsBean("userDao"));
 
@@ -21,8 +27,6 @@ class SpringIoCTest {
 
     @Test
     void setterBasedDependencyInjection() {
-        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("beans.xml");
-
         UserServiceImpl userService = (UserServiceImpl) ac.getBean("userService");
 
         Assertions.assertInstanceOf(SimpleUserDao.class, userService.getUserDao());
@@ -30,8 +34,6 @@ class SpringIoCTest {
 
     @Test
     void constructorBasedDependencyInjection() {
-        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("beans.xml");
-
         UserServiceImpl userService = ac.getBean("userService2", UserServiceImpl.class);
 
         Assertions.assertInstanceOf(NopeUserDao.class, userService.getUserDao2());
@@ -39,7 +41,6 @@ class SpringIoCTest {
 
     @Test
     void initAndDestroyMethod() {
-        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("beans.xml");
         //ac.close();
         ac.registerShutdownHook();
     }
