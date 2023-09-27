3b23f3fa97ad09be836cadb48866d148462ec0f9
ziorye <ziorye@gmail.com>
2023-09-27 21:17:02 +0800

    user/body-pojo
    @EnableWebMvc
    @RequestBody
    jackson-databind

--- day0928-spring-mvc/src/main/java/com/ioewvsau/controller/UserController.java
+++ day0928-spring-mvc/src/main/java/com/ioewvsau/controller/UserController.java
@@ -4,6 +4,7 @@ import com.ioewvsau.pojo.User;
 import com.ioewvsau.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -36,5 +37,11 @@ public class UserController {
     @ResponseBody
     String paramPojo(User user) {
         return "UserController@paramPojo---" + user;
+    }
+
+    @RequestMapping("/user/body-pojo")
+    @ResponseBody
+    String bodyPojo(@RequestBody User user) {
+        return "UserController@bodyPojo---" + user;
     }
 }
