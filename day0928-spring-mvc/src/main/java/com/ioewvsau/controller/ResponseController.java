c60174fc5f7a12f7f6496ee88dff21de4579a960
ziorye <ziorye@gmail.com>
2023-09-27 21:19:02 +0800

    /response/jsonList
    HttpMessageConverter

--- day0928-spring-mvc/src/main/java/com/ioewvsau/controller/ResponseController.java
+++ day0928-spring-mvc/src/main/java/com/ioewvsau/controller/ResponseController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/response")
 public class ResponseController {
@@ -26,5 +29,21 @@ public class ResponseController {
         user.setName("Alice");
         user.setEmail("alice@example.com");
         return user;
+    }
+
+    @RequestMapping("/jsonList")
+    @ResponseBody
+    public List<User> responseJsonList() {
+        ArrayList<User> users = new ArrayList<>();
+
+        for (int i = 0; i < 2; i++) {
+            User user = new User();
+            user.setName("Alice" + i);
+            user.setEmail("alice"+i+"@example.com");
+
+            users.add(user);
+        }
+
+        return users;
     }
 }
