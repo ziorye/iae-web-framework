1e91be03ce9170c8a814e88efc20aef2c4bff736
ziorye <ziorye@gmail.com>
2023-09-27 09:19:33 +0800

    AOP
    Advice - @Before

---
+++ day0926-x-spring-aop/src/main/java/com/ioewvsau/config/DemoAopConfig.java
@@ -0,0 +1,11 @@
+package com.ioewvsau.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@ComponentScan("com.ioewvsau")
+@EnableAspectJAutoProxy
+public class DemoAopConfig {
+}
---
+++ day0926-x-spring-aop/src/test/java/com/ioewvsau/config/DemoAopConfigTest.java
@@ -0,0 +1,16 @@
+package com.ioewvsau.config;
+
+import com.ioewvsau.dao.UserDao;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+
+class DemoAopConfigTest {
+    @Test
+    void startTimeUsingAtBefore() {
+        AbstractApplicationContext applicationContext = new AnnotationConfigApplicationContext(DemoAopConfig.class);
+        UserDao userDao = applicationContext.getBean(UserDao.class);
+
+        userDao.retrieve();
+    }
+}
