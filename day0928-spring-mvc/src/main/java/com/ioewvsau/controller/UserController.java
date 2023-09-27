f1574c264e245c07db28e8d0a800351c5df20893
ziorye <ziorye@gmail.com>
2023-09-27 21:16:05 +0800

    user/param-not-match
    @RequestParam("name")

--- day0928-spring-mvc/src/main/java/com/ioewvsau/controller/UserController.java
+++ day0928-spring-mvc/src/main/java/com/ioewvsau/controller/UserController.java
@@ -4,6 +4,7 @@ import com.ioewvsau.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -22,5 +23,11 @@ public class UserController {
     @ResponseBody
     String param(String name, String email) {
         return "UserController@param---" + name + ", " + email;
+    }
+
+    @RequestMapping("/user/param-not-match")
+    @ResponseBody
+    String paramNotMatch(@RequestParam("name") String userName, String email) {
+        return "UserController@paramNotMatch---" + userName + ", " + email;
     }
 }
