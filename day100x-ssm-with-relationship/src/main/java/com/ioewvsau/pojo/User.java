ee2573448fb614ed76305781708368135bd5af39
ziorye <ziorye@gmail.com>
2023-10-05 16:32:25 +0800

    ssm-with-relationship ToMany
    user hasMany posts

--- day100x-ssm-with-relationship/src/main/java/com/ioewvsau/pojo/User.java
+++ day100x-ssm-with-relationship/src/main/java/com/ioewvsau/pojo/User.java
@@ -2,6 +2,8 @@ package com.ioewvsau.pojo;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class User {
 	private Long id;
@@ -9,4 +11,6 @@ public class User {
 	private String email;
 	private String password;
 	private int status;
+
+	List<Post> posts;
 }
